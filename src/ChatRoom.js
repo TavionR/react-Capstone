ff6e0f691cf6390ab5f3f99496ba3e343a041645
TavionR <Tavion_r@ymail.com>
2023-11-19 12:21:27 -0600

    changed the layout of chatroom

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,16 +1,24 @@
-// src/ChatRoom.js
-import React, { useState } from 'react';
+// ChatRoom.js
+
+import React, { useState, useEffect } from 'react';
+import './ChatRoom.css'; // Import the CSS file
 
 const ChatRoom = ({ user, socket }) => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
   const [file, setFile] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedFriend, setSelectedFriend] = useState(null);
+  const [lastSentMessage, setLastSentMessage] = useState(null);
+  const friends = ['Friend 1', 'Friend 2', 'Friend 3']; // Replace with your actual list of friends
 
   const sendMessage = (e) => {
     e.preventDefault();
     if (message.trim() !== '') {
-      socket.emit('chat message', { user, message, type: 'text' });
+      const newMessage = { user, message, type: 'text', to: selectedFriend };
+      socket.emit('chat message', newMessage);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      setLastSentMessage(message); // Set the last sent message
       setMessage('');
     }
   };
@@ -25,7 +33,9 @@ const ChatRoom = ({ user, socket }) => {
       const reader = new FileReader();
       reader.onload = (e) => {
         const fileData = e.target.result;
-        socket.emit('file upload', { user, fileData, fileName: file.name, type: 'file' });
+        const newFileMessage = { user, fileData, fileName: file.name, type: 'file', to: selectedFriend };
+        socket.emit('file upload', newFileMessage);
+        setMessages((prevMessages) => [...prevMessages, newFileMessage]);
       };
       reader.readAsDataURL(file);
     }
@@ -41,6 +51,10 @@ const ChatRoom = ({ user, socket }) => {
     console.log('Searching for:', searchTerm);
   };
 
+  const handleFriendClick = (friend) => {
+    setSelectedFriend(friend);
+  };
+
   socket.on('chat message', (msg) => {
     setMessages((prevMessages) => [...prevMessages, msg]);
   });
@@ -49,8 +63,17 @@ const ChatRoom = ({ user, socket }) => {
     setMessages((prevMessages) => [...prevMessages, uploadedFile]);
   });
 
+  useEffect(() => {
+    // Clear last sent message after a delay
+    const timeout = setTimeout(() => {
+      setLastSentMessage(null);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [lastSentMessage]);
+
   return (
-    <div>
+    <div className="chat-room-container">
       <nav className="navbar">
         <div className="nav-left">
           <span className="nav-item">Chat Room</span>
@@ -68,29 +91,43 @@ const ChatRoom = ({ user, socket }) => {
         </div>
       </nav>
       <div className="chat-container">
-        <h3>Welcome, {user}!</h3>
-        <ul>
-          {messages.map((msg, index) => (
-            <li key={index}>
-              {msg.type === 'text' ? (
-                <span>{msg.user}: {msg.message}</span>
-              ) : (
-                <span>{msg.user} uploaded a file: {msg.fileName}</span>
-              )}
-            </li>
-          ))}
-        </ul>
-        <form onSubmit={sendMessage}>
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="Type your message..."
-          />
-          <button type="submit">Send</button>
-        </form>
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={uploadFile}>Upload File</button>
+        <div className="sidebar">
+          <h3>Friends</h3>
+          <ul>
+            {friends.map((friend, index) => (
+              <li key={index} onClick={() => handleFriendClick(friend)} className={selectedFriend === friend ? 'selected' : ''}>
+                {friend}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="chat-content">
+          <h3>Welcome, {user}!</h3>
+          <ul>
+            {messages.map((msg, index) => (
+              <li key={index}>
+                {msg.type === 'text' ? (
+                  <span>{msg.user}: {msg.message}</span>
+                ) : (
+                  <span>{msg.user} uploaded a file: {msg.fileName}</span>
+                )}
+              </li>
+            ))}
+          </ul>
+          {/* Display the last sent message */}
+          {lastSentMessage && <div className="last-sent-message">{`Last Sent: ${lastSentMessage}`}</div>}
+          <form onSubmit={sendMessage}>
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Type your message..."
+            />
+            <button type="submit">Send</button>
+          </form>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={uploadFile}>Upload File</button>
+        </div>
       </div>
     </div>
   );
